2df965d1809aa64fbf0005761833c28822b2b327
Isaiah941 <mayerisaiah281@gmail.com>
2025-07-15 18:26:55 -0400

    Delete overtherainbow directory
    bc I want to

--- overtherainbow/js/footer.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-	fetch("/includes/footer.html") // Removed leading slash
-		.then(res => res.text())
-		.then(html => {
-			document.getElementById("footer").innerHTML = html;
-		});
-});
